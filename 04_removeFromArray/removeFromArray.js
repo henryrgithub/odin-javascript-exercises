163b0fb9d569fd5480301a047401909b571effd9
henryrgithub <hroeramsey@gmail.com>
2023-07-19 11:36:25 -0700

    solved remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arrIn, ...indices) {
+    let retArr = arrIn;
+    for(const index of indices){
+        retArr = retArr.filter(item => item !== index);
+    }
+    return retArr;
 };
 
 // Do not edit below this line
