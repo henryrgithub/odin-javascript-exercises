15e64a39e5873217f25cb778e5790b90351e3e26
henryrgithub <hroeramsey@gmail.com>
2023-07-19 12:32:45 -0700

    solved sum all exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(inOne, inTwo) {
+    if ((typeof(inOne)!=='number')||(typeof(inTwo)!=='number')) return 'ERROR';
+    let small = Math.min(inOne,inTwo);
+    if (small < 0) return('ERROR')
+    let large = Math.max(inOne,inTwo);
+    let retnum = 0;
+    for(let i = small; i<= large; i++) retnum += i;
+    return retnum;
 };
 
 // Do not edit below this line
