543bfc690b4b0f58b5137b7c4a5c2ea81bbafeca
henryrgithub <hroeramsey@gmail.com>
2023-07-19 12:42:24 -0700

    solved temp conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenIn) {
+  return Math.round((farenIn-32)*50/9)/10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcIn) {
+  return Math.round((celcIn*9/5+32)*10)/10
 };
 
 // Do not edit below this line
