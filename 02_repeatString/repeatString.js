74c49007dd29fb55031436c06946c1ed4702da31
henryrgithub <hroeramsey@gmail.com>
2023-07-19 11:14:46 -0700

    passed repeatstring. Also just committing package lock, w/e

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(strIn,numIn) {
+    let retStr = '';
+    if(numIn===0);
+    else if (numIn>0){
+        for(let i = 0; i < numIn; i++) retStr = retStr.concat(strIn);
+    }
+    else retStr = 'ERROR';
+    return retStr
 };
 
 // Do not edit below this line
