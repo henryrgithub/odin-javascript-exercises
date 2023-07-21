322784b08605524a0979c86760028e3cb58317e9
henryrgithub <hroeramsey@gmail.com>
2023-07-21 08:10:38 -0700

    solved exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,28 @@
-const add = function() {
+const add = function(in1, in2) {
+  return in1+in2
+};
+
+const subtract = function(in1, in2) {
+  return in1-in2
+};
+
+const sum = function(ins) {
+  return ins.reduce((summer,curr) => summer + curr, 0) 
+};
+
+const multiply = function(...ins) {
+  return ins.reduce((prod,curr) => prod*curr,1)
+
+};
+
+const power = function(in1, in2) {
+  return in1**in2
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function(numIn) {
+  if(numIn < 2) return 1;
+  return numIn*factorial(numIn - 1);
 	
 };
 
