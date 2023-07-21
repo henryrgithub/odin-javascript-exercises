26b6ab8a140d53fbe3ea1e823652b225604775c7
henryrgithub <hroeramsey@gmail.com>
2023-07-21 09:07:59 -0700

    solved 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(peeps) {
+    const currYear = new Date().getFullYear();
+    const oldest = peeps.reduce((oldie,curr) => {
+        if(!Object.keys(oldie).length) {
+            const test = curr;
+            return test;
+        }
+        const currDeath = curr.yearOfDeath || currYear;
+        const currAge = currDeath - curr.yearOfBirth;
+        const prevDeath = oldie.yearOfDeath || currYear;
+        const prevAge = prevDeath - oldie.yearOfBirth;
+        if (currAge > prevAge) {
+            const test = curr;
+            return test;
+        } else {
+            const test = oldie;
+            return test;
+        }
+    },
+    {})
+    console.log(oldest.name)
+    return oldest
+
 
 };
 
