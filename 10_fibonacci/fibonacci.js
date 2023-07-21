5f0418b54787d3eb12479e86167e72351d7ce4ab
henryrgithub <hroeramsey@gmail.com>
2023-07-21 08:38:09 -0700

    solved 10-fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(fibInd) {
+    if(fibInd < 1) return("OOPS");
+    let fibNum = 1
+    let prevFib = 0
+    for (let i = 0; i < fibInd-1; i++){
+        const newFib = fibNum + prevFib
+        prevFib = fibNum
+        fibNum = newFib;
+    }
+    return fibNum
 
 };
 
