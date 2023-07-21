5283b4763f9a32bcc27734a9e2b8cf9e9e56a5de
henryrgithub <hroeramsey@gmail.com>
2023-07-21 08:25:25 -0700

    solved 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,16 @@
-const palindromes = function () {
+const palindromes = function (palIn) {
+    const regex = /[.,\/#!$%\^&\*;:{}=\-_`~()]/g;
+    const regex2 = /\s/g;
+    const palSplit = palIn.replace(regex, '').replace(regex2, '').toLowerCase().split('');
+    console.log(palSplit);
+    let startInd = 0;
+    let endInd = palSplit.length-1;
+    while(startInd<endInd){
+        if(palSplit[startInd] !== palSplit[endInd]) return false;
+        startInd++;
+        endInd--;
+    }
+    return true;
 
 };
 
